638feb3ab62b06a27a33d01df08841c03a18c498
Viktor Oshkalo <vvicmail@gmail.com>
2018-06-04 01:36:34 +0300

    Add project files.

---
+++ CompanyApp.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CompanyApp.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.That(0, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            Assert.That(0, Is.EqualTo(1));
+        }
+    }
+}
---
+++ CompanyApp/Models/Company.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyApp.Models
+{
+    public class Company
+    {
+        public List<Employee> Employees { get; } = new List<Employee>();
+
+        public static Company companyInstance { get; private set; }
+
+        static Company()
+        {
+            companyInstance = new Company();
+        }
+
+        private Company()   // Lets say this is singleton
+        {
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            if (employee is ManagerAbstract)
+            {
+                foreach (var subordinate in (employee as ManagerAbstract).SubordinateEmployees)
+                {
+                    try
+                    {
+                        AddEmployee(subordinate);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+
+            if (!Employees.Contains(employee))
+                Employees.Add(employee);
+            else
+                throw new Exception(message: "Employee already exist in current list: " + employee.Name + ", ID: " + employee.ID);
+        }
+
+        public double GetTotalSalary()
+        {
+            return Employees.Sum(emp => emp.CalcSalary());
+        }
+    }
+}
